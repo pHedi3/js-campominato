84dc38786652ce4bf91f1b41cf299c8480d81076
pHedi3 <federico.gualdi3@gmail.com>
2021-06-11 11:23:22 +0200

    risoluzione problema

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ var numBox = 0
 var bombNumList = [];
 var userNumList = [];
 var dif = prompt('inserisci difficoltà tra 0, 1 e 2')
+var end = false;
 
 switch (dif) {
     case '0':
@@ -36,15 +37,22 @@ console.log(bombNumList)
 
 while (userNumList.length < (numBox - numBomb)) {
     var userNum = parseInt(prompt('inserisci un numero da 1 a ' + numBox))
-    if(isNaN(userNum)) {
-        alert('devi inserire un numero')
+    if(isNaN(userNum) || userNum < 1 || userNum > numBox) {
+        alert('devi inserire un numero corretto')
     } else if (!userNumList.includes(userNum) && !bombNumList.includes(userNum)) {
         userNumList.push(userNum)
     } else if (bombNumList.includes(userNum)) {
+        end = true
         break
     } else {
         alert('numero gia inserito')
     }
 }
 
-document.getElementById('result').innerHTML = 'il tuo punteggio è: ' + userNumList.length;
+if(!end) {
+    document.getElementById('result').innerHTML += 'hai vinto'
+} else {
+    document.getElementById('result').innerHTML += 'hai preso la mina'
+}
+
+document.getElementById('result').innerHTML += '<br>il tuo punteggio è: ' + userNumList.length;
